508376f73b1a17d5f6f5ee23ed21390a8a0d0b6c
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-11-30 18:39:26 +0100

    Update Agent /Tracing

--- src/Application/Agents/Agent.cs
+++ src/Application/Agents/Agent.cs
@@ -8,27 +8,24 @@ namespace Application.Agents;
 
 public class Agent(AIAgent agent, IAgentThreadRepository repository, AgentTypes type) : IAgent
 {
-    private Activity? _activity;
-    
+    private const string AgentTelemetryName = "Agent";
     public async Task<AgentRunResponse> RunAsync(
         IEnumerable<ChatMessage> messages,
         Guid sessionId,
         Guid userId,
         CancellationToken cancellationToken = default)
     {
-        Trace();
+        using var activity = Telemetry.Start(AgentTelemetryName);
 
         var thread = await LoadAsync(userId, sessionId, type);
 
         var response =  await agent.RunAsync(messages, thread, cancellationToken: cancellationToken);
 
-        Trace(response);
+        TraceTokenUsage(response, activity);
         
         var threadState = thread.Serialize();
 
         await repository.SaveAsync(userId, sessionId, new AgentState(threadState), type.ToString());
-
-        TraceEnd();
 
         return response;
     }
@@ -39,38 +36,26 @@ public class Agent(AIAgent agent, IAgentThreadRepository repository, AgentTypes 
         Guid userId,
         CancellationToken cancellationToken = default)
     {
-        Trace();
+        using var activity = Telemetry.Start(AgentTelemetryName);
 
         var thread = await LoadAsync(userId, sessionId, type);
 
         var response = await agent.RunAsync(message, thread, cancellationToken: cancellationToken);
 
-        Trace(response);
+        TraceTokenUsage(response, activity);
 
         var threadState = thread.Serialize();
 
         await repository.SaveAsync(userId, sessionId, new AgentState(threadState), type.ToString());
 
-        TraceEnd();
-
         return response;
     }
 
-    private void Trace()
+    private static void TraceTokenUsage(AgentRunResponse response, Activity? activity)
     {
-        _activity = Telemetry.Start("Agent");
-    }
-
-    private void TraceEnd()
-    {
-        _activity?.Dispose();
-    }
-
-    private void Trace(AgentRunResponse response)
-    {
-        _activity?.SetTag("llm.input_tokens", response.Usage?.InputTokenCount ?? 0);
-        _activity?.SetTag("llm.output_tokens", response.Usage?.OutputTokenCount ?? 0);
-        _activity?.SetTag("llm.total_tokens", response.Usage?.TotalTokenCount ?? 0);
+        activity?.SetTag("llm.input_tokens", response.Usage?.InputTokenCount ?? 0);
+        activity?.SetTag("llm.output_tokens", response.Usage?.OutputTokenCount ?? 0);
+        activity?.SetTag("llm.total_tokens", response.Usage?.TotalTokenCount ?? 0);
     }
 
     private async Task<AgentThread> LoadAsync(Guid userId, Guid sessionId, AgentTypes agentType)
