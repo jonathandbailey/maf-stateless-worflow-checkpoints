3cfd2bcf12ac137903a9c7bf93a4e324fe09b47e
Jonathan Bailey <jonathan.d.bailey@gmail.com>
2025-12-12 13:14:39 +0100

    Add popup for agent feedback

--- ui/src/components/chat/AgentFeedback.tsx
+++ ui/src/components/chat/AgentFeedback.tsx
@@ -1,6 +1,7 @@
-import { Flex, Spin } from "antd";
+import { Flex, Popover, Spin } from "antd";
 import type { UIExchange } from "../../types/ui/UIExchange";
 import type { Status } from "../../types/ui/Status";
+import { OpenAIOutlined } from "@ant-design/icons";
 
 interface AgentFeedbackProps {
     message: UIExchange;
@@ -10,11 +11,22 @@ interface AgentFeedbackProps {
 const AgentFeedback = ({ message, status }: AgentFeedbackProps) => {
     return (
         <> <Flex vertical>
-            <div>
-                {message?.assistant.text}
+            <div style={{ marginBottom: "24px", marginLeft: "24px", height: "32px" }} >
+                {!message?.assistant.isLoading && (
+                    <Flex>
+                        <Popover open={true}
+                            placement="right"
+                            content={<div style={{ maxWidth: "600px" }}>{message?.assistant.text}</div>}
+                        >
+                            <OpenAIOutlined />
+                        </Popover>
+
+                        { }
+                    </Flex>)}
+
             </div>
             {message?.assistant.isLoading && (
-                <Flex align="center" gap="small" >
+                <Flex align="center" gap="small" style={{ padding: "16px" }}>
                     <Spin size="small" />
                     {status?.message && (
                         <span style={{ fontSize: '14px', color: '#666' }}>
@@ -23,7 +35,7 @@ const AgentFeedback = ({ message, status }: AgentFeedbackProps) => {
                     )}
                 </Flex>
             )}
-        </Flex>
+        </Flex >
         </>
     );
 }
